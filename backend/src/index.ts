370344fe5ca182b8cfd0952b24879ee42d54af5a
baijanathTharu <bnchaudhary.tharu@gmail.com>
2023-04-29 14:25:51 +0545

    created apis for todo

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express, NextFunction, Request, Response } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import { db } from "./db";
 
 dotenv.config();
 
@@ -16,6 +17,94 @@ app.use(
 app.get("/ping", (req: Request, res: Response) => {
   res.status(200).json({ message: "pong" });
 });
+
+app.post(`/todos`, async (req: Request, res: Response, next: NextFunction) => {
+  const body = req.body as { title: string };
+
+  if (!body.title) {
+    next({
+      mesage: "Title not send",
+      status: 400,
+    });
+    return;
+  }
+
+  const created = await db.todo.create({
+    data: {
+      title: body.title,
+    },
+  });
+
+  res.status(201).json({
+    id: created.id,
+    title: created.title,
+  });
+});
+
+app.get(`/todos`, async (req: Request, res: Response) => {
+  const todos = await db.todo.findMany();
+  res.status(200).json(
+    todos.map((todo) => ({
+      id: todo.id,
+      title: todo.title,
+    }))
+  );
+});
+
+app.delete(
+  `/todos/:id`,
+  async (req: Request, res: Response, next: NextFunction) => {
+    const id = req.params?.id;
+    if (!id) {
+      next({
+        message: "Id not sent",
+        status: 400,
+      });
+      return;
+    }
+
+    const todo = await db.todo.findFirst({
+      where: {
+        id: Number(id),
+      },
+    });
+
+    if (!todo?.id) {
+      next({
+        message: "Id not found",
+        status: 404,
+      });
+      return;
+    }
+
+    await db.todo.delete({
+      where: {
+        id: Number(id),
+      },
+    });
+
+    res.status(200).json({ message: "Deleted" });
+  }
+);
+
+app.use(
+  (
+    error: {
+      message?: string;
+      status?: number;
+    },
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    const e = {
+      message: error.message || "Something went wrong",
+      status: error.status || 500,
+    };
+    console.log(`error ${error.message}`);
+    res.status(e.status).send(e.message);
+  }
+);
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
